ed51a482551bf1fa7d95b9151598981e8ccd4319
Jesusaed19 <jesusarmandoed57@gmail.com>
2023-09-29 14:59:17 -0400

    Creacion de Routers

---
+++ routers/index.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+
+router = APIRouter(tags=['Index'], responses={404: {"Error": "No se encontro"}})
+
+router.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
---
+++ routers/router_email.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from schemas.email import Email
+from lib.email import email
+
+router = APIRouter(tags=['Email'], responses={404: {"Error": "No se encontro"}})
+
+
+router.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@router.post('/thanks', response_class=HTMLResponse)
+async def dataemail(request: Request, data_email = Email, name: str = Form(), mail: str = Form(), project: str = Form()):
+    data_email.name = name
+    data_email.mail = mail
+    data_email.project = project
+    
+    email(data_email.mail, data_email.name, data_email.project)
+    
+    return templates.TemplateResponse("contact.html", {"request": request})
+    # return Response(status_code=status.HTTP_204_NO_CONTENT)
